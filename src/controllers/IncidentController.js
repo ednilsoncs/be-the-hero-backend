e804464af815b68fc9db7f955c8fee60522be845
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2020-04-14 00:58:19 -0300

    feat: inital structure of project

---
+++ src/controllers/IncidentController.js
@@ -0,0 +1,42 @@
+const connection  = require('../database/connection');
+
+module.exports = {
+  
+  async index(request, response){
+    const { page = 1 } = request.query
+    const  [ count ] = await connection('incidents').count();
+    const incidents = await connection('incidents')
+     .join('ongs','ongs.id','=','incidents.ong_id')
+     .limit(5)
+     .offset((page-1)*5) 
+     .select(['incidents.*','ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+
+    response.header('X-Total-Count', count['count(*)']) 
+    return response.json(incidents); 
+  },
+
+  async create(request, response){
+    const {body : { title, description , value}, headers:{ authorization: ong_id }} = request;
+    const [ id ] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_id,
+    })
+    return response.json({ id })
+  },
+
+  async delete(request,response){
+    const { params: { id }, headers: { authorization: ong_id } } = request;
+    const incident  = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+    if(incident.ong_id !== ong_id){
+      return response.status(401).json({error: 'Operation not permitted'})
+    } 
+    await connection('incidents').where('id',id).delete();
+    return response.status(204).send();
+  }
+
+}
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const OngController = require('./controllers/OngControllers');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController =  require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+const routes  = express.Router();
+
+routes.post('/sessions',SessionController.create);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+routes.get('/profile', ProfileController.index);
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
